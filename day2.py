8d99ae8d0dc35e70a0890703c466655bbeb297ae
tmengineerx <tm.engineer.x@gmail.com>
2025-12-23 19:29:36 +0900

    fix: インデントエラーを修正してfor文を完成

---
+++ day2.py
@@ -0,0 +1,10 @@
+#顧客名と受注確立(%)のリスト
+customers = [["A社",80],["B社",30],["C社",100],["D社",10]]
+
+for customer in customers:
+    name = customer[0]
+    chance = customer[1]
+    if chance >= 50:
+        print (f"{name}様：至急アプローチ！")
+    else:
+        print(f"{name}様：時期尚早")
